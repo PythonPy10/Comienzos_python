14fe0f7f43819a1ebfd8fdf3e457ca98429b4afc
Angie <91624845+natalynohava@users.noreply.github.com>
2024-08-13 16:26:13 +0000

    mas ejercicios de condicionales

---
+++ ejrcicios_calse2.py
@@ -0,0 +1,21 @@
+num1 = int(input("Ingrese primer numero "))
+num2 = int(input("Ingrese segundo numero "))
+
+if(num1> num2):
+    print("El primer numero es mayor")
+else:
+    print("El segundo es mayor")
+
+num1 = int(input("Ingrese un numero"))
+
+if(num1%2==0):
+    print("El numero es par")
+else:
+    print("El numero es impar")
+
+num = int(input("Ingrese un numero "))
+
+if(10 <= num <= 20):
+    print("Si está en el rango")
+else:
+    print("No está en el rango")
